ffbf3f2f2d74295983a489a497c26bed6d39d63c
Ashwin DS <astroashwin@outlook.com>
2022-02-11 15:33:00 +0530

    mojito: Build GcamGOPrebuilt

--- device.mk
+++ device.mk
@@ -155,6 +155,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# GCamGo
+PRODUCT_PACKAGES += \
+    GCamGOPrebuilt-V2
+
 # Charger
 PRODUCT_PACKAGES += \
     libsuspend
