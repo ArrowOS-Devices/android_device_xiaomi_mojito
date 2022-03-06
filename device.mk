3582fd82949227a00fdb38a447ecd588a96d1bac
Alexander Koskovich <akoskovich@pm.me>
2022-03-06 02:11:47 -0700

    mojito: Update to Boot Control HAL v1.2
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -21,9 +21,9 @@ $(call inherit-product, \
 TARGET_IS_VAB := true
 
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.boot@1.2-service
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
