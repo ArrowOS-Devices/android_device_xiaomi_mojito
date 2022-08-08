11641ac5d423274a8556f2c59b8d81fb801b6be2
Cyber Knight <cyberknight755@gmail.com>
2022-08-09 02:13:33 +0800

    mojito: Drop ZRAM
    - We use vbswap instead of zram in our kernel anyways.
    
    Co-authored-by: Alex Winkowski <dereference23@outlook.com>
    Signed-off-by: Alex Winkowski <dereference23@outlook.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Change-Id: If74a23a42adae6d549d6dcdf0aa9d92e336ca438

--- device.mk
+++ device.mk
@@ -399,7 +399,6 @@ PRODUCT_PACKAGES += \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.sh \
-    fstab.zram \
     fstab.qcom \
     init.qcom.power.rc \
     init.qcom.rc \
