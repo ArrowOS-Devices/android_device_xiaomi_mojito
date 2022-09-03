1b19d2cec993cebdf97e2ba53ed5c806006b3ed1
Sarthak Roy <sarthakroy2002@gmail.com>
2022-09-03 17:45:06 +0530

    mojito: Move props to /configs/props
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I71d7f722780b1cef269dfc7a82e1692c265248f8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -167,11 +167,11 @@ BOARD_USES_QCOM_HARDWARE := true
 TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 
 # Properties
-TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
-TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
-TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
-TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
+TARGET_ODM_PROP += $(DEVICE_PATH)/configs/props/odm.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/props/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/props/vendor.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/props/product.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/configs/props/system_ext.prop
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab_AB.qcom
