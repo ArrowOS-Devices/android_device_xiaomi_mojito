c993223abedffb3c6d2cd65064aacf739dafaf0f
Nauval Rizky <enuma.alrizky@gmail.com>
2022-12-03 21:12:10 +0530

    mojito: power: Add custom boost extension for scroll handling
    Currently, we're not using CAF's BoostFramework due to having a pure
    AOSP based source, thus qcom perf hinting are not correctly applied.
    This extension will handle the vertical fling through device-specific
    boost override, utilizing existing libqti-perfd hinting.
    So any further configuration should be happens inside vertical scroll
    config inside perfboostsconfig.xml
    
    Reference: vendor/qcom/opensource/power/power-6150.c w/ reduced
    max interactive timeout
    
    Change-Id: I823c9383041b68ff2ce62d5f1a1c5091b2dfac15

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -161,6 +161,7 @@ BOARD_USES_QCOM_HARDWARE := true
 # Power
 TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 TARGET_USES_NON_LEGACY_POWERHAL := true
+TARGET_POWERHAL_BOOST_EXT := $(DEVICE_PATH)/power/boost-ext.cpp
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/configs/props/odm.prop
